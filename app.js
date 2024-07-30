cef763415beaa60ea54e5daf07d5fa9f203be5cc
Israel Rotimi <130306892+israelrotimi@users.noreply.github.com>
2024-07-30 02:17:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
-//Toggle menu for mobile screen
-const navbar = document.getElementsByClassName('nav-mobile');
-const menuBtn = document.getElementById('menu-btn');
-const menu = document.getElementById('menu');
-menuBtn.addEventListener('click', toggleMenu);
-
-function toggleMenu(){
-    console.log('display was set to: ' + menu.style.display);
-    if(menu.style.display === 'none' || menu.style.display === ''){
-        menu.style.display = 'block';
-    } else {
-        menu.style.display = 'none';
-    }
-    console.log('display is set to: ' + menu.style.display);
-}
-//add chevron icon to dropdown trigger
-//<i class="bi-chevron-right"></i>
+document.querySelector('.nav-toggle').addEventListener('click', function() {
+    document.querySelector('.nav-links').classList.toggle('active');
+});
